9dca5c6f56e2322ba71365f43bb11d8f21f06076
ljwvv666 <1052045941@qq.com>
2024-11-18 12:00:44 +0800

    ufp总和

--- src/stores/ufpClass.ts
+++ src/stores/ufpClass.ts
@@ -3,12 +3,12 @@ import { ref, computed } from "vue";
 
 export const useUfpStore = defineStore("ufpStore", () => {
   // 定义五个响应式值
-  const EO = ref<number>(1);
-  const EI = ref<number>(1);
-  const EQ = ref<number>(1);
-  const ILF = ref<number>(1);
-  const EIF = ref<number>(1);
-
+  const EO = ref<number>(0);
+  const EI = ref<number>(0);
+  const EQ = ref<number>(0);
+  const ILF = ref<number>(0);
+  const EIF = ref<number>(0);
+  const UFP = ref<number>(0);
   // 计算属性：计算总和（可选）
   const total = computed(() => EO.value + EI.value + EQ.value + ILF.value + EIF.value);
 
@@ -28,6 +28,9 @@ export const useUfpStore = defineStore("ufpStore", () => {
   const setEIF = (value: number) => {
     EIF.value = value;
   };
+  const setUFP = (value: number) => {
+    UFP.value = value;
+  };  
 
   // 导出 state 和方法
   return {
@@ -37,10 +40,12 @@ export const useUfpStore = defineStore("ufpStore", () => {
     ILF,
     EIF,
     total,
+    UFP,
     setEO,
     setEI,
     setEQ,
     setILF,
     setEIF,
+    setUFP
   };
 });
